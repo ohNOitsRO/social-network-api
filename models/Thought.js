795ef919294746b13f64755c196f62aad7c5885c
Havok <rngdhavok@hotmail.com>
2023-04-17 19:32:17 -0400

    more models work

--- models/Thought.js
+++ models/Thought.js
@@ -1,24 +1,52 @@
 const mongoose = require('mongoose');
 
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+        },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        },
+    username: {
+        type: String,
+        required: true,
+        },
+    createdAt: {
+        type: Date,
+        default: Date.now, 
+        get: (timeCreated) => dateFormat(timeCreated),
+      },
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { 
-            type: String, 
-            required: true,
-            minlength: 1,
-            maxlength: 280
-            },
+        type: String, 
+        required: true,
+        minlength: 1,
+        maxlength: 280
+        },
     createdAt: { 
-            type: Date,
-            default: Date.now,
-            get: (timeCreated) => dateFormat(timeCreated)
-            },
+        type: Date,
+        default: Date.now,
+        get: (timeCreated) => dateFormat(timeCreated)
+        },
     username: [{ 
-            type: String, 
-            ref: 'thought' 
-            }],
+        type: String, 
+        ref: 'thought' 
+        }],
     reactions: [ 
-            ReactionSchema 
-            ]
+        reactionSchema 
+        ]
 },
 {
     toJSON: {
@@ -32,6 +60,6 @@ usersSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
   });
 
-const Thought = model("Thought", ThoughtSchema);
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
--- models/Users.js
+++ models/Users.js
@@ -2,31 +2,31 @@ const mongoose = require('mongoose');
 
 const usersSchema = new mongoose.Schema({
   username: { 
-          type: String, 
-          required: true, 
-          unique: true, 
-          trim: true 
-          },
+      type: String, 
+      required: true, 
+      unique: true, 
+      trim: true 
+      },
   email: { 
-          type: String, 
-          required: true, 
-          unique: true, 
-          match: [/.+\@.+\..+/] 
-          },
+      type: String, 
+      required: true, 
+      unique: true, 
+      match: [/.+\@.+\..+/] 
+      },
   thought: [{ 
-          type: Schema.Types.ObjectId, 
-          ref: 'thought' 
-          }],
+      type: Schema.Types.ObjectId, 
+      ref: 'thought' 
+      }],
   friends: [{ 
-          type: Schema.Types.ObjectId, 
-          ref: 'user' 
-          }],
+      type: Schema.Types.ObjectId, 
+      ref: 'user' 
+      }],
   },
   {
   toJSON: {
-          virtuals: true,
-          getters: true
-          },
+      virtuals: true,
+      getters: true
+      },
   id: false
   }
 );
@@ -37,6 +37,5 @@ usersSchema.virtual('friendsCount').get(function () {
 
   
 const Users = mongoose.model('Users', userSchema);
-
 
 module.exports = Users;
