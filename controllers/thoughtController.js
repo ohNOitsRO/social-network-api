690bdcde20535a1c17c39f7b91fd5749b3343a92
Havok <rngdhavok@hotmail.com>
2023-04-17 20:20:03 -0400

    thought controller work

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -18,10 +18,32 @@ const thoughtController = {
         res.sendStatus(400);
       });
   },
-
-
-
-
-
   
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: 'reactions',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(thoughtData => {
+        if (!thoughtData) {
+          res.status(404).json({ message: 'This ID has no thoughts!' });
+          return;
+        }
+        res.json(thoughtData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+
+
+
+
+
+
 }
